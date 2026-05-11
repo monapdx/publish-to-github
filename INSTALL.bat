8f3999d501901e891807427f8ab731c435d8c2ce
Ashly <thegirlnextfloor@gmail.com>
2026-05-11 10:25:12 -0700

    bat files

---
+++ INSTALL.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Installing Publish to GitHub...
+echo.
+
+npm install
+
+echo.
+echo Done!
+echo You can now run start.bat to open the editor.
+pause
---
+++ START.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Starting Publish to GitHub...
+echo.
+
+start "" http://localhost:5173
+npm run dev
+
+pause
